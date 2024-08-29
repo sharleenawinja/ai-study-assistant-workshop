d1711d27e8985523bf6dbd80b886d348b4243433
sharleenawinja <awinjasharleen@gmail.com>
2024-08-29 22:52:27 +0300

    modify icons

--- src/components/icons/DraftIcon.tsx
+++ src/components/icons/DraftIcon.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
 
-export type DraftIconProps = React.SVGAttributes<SVGElement>
+export type DraftIconProps = React.SVGAttributes<SVGElement> & {
+  isFilterIcon?: boolean
+}
 
-export const DraftIcon: React.FC<DraftIconProps> = ({ ...props }) => {
-  return (
+export const DraftIcon: React.FC<DraftIconProps> = ({
+  isFilterIcon,
+  ...props
+}) => {
+  const defaultIcon = (
     <svg
       xmlns="http://www.w3.org/2000/svg"
       height="24px"
@@ -14,4 +19,28 @@ export const DraftIcon: React.FC<DraftIconProps> = ({ ...props }) => {
       <path d="M240-80q-33 0-56.5-23.5T160-160v-640q0-33 23.5-56.5T240-880h320l240 240v480q0 33-23.5 56.5T720-80H240Zm280-520v-200H240v640h480v-440H520ZM240-800v200-200 640-640Z" />
     </svg>
   )
+
+  const filterIcon = (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      x="0px"
+      y="0px"
+      width="100"
+      height="100"
+      viewBox="0 0 48 48"
+    >
+      <path
+        fill="#2196f3"
+        d="M37,45H11c-1.657,0-3-1.343-3-3V6c0-1.657,1.343-3,3-3h19l10,10v29C40,43.657,38.657,45,37,45z"
+      ></path>
+      <path fill="#bbdefb" d="M40 13L30 13 30 3z"></path>
+      <path fill="#1565c0" d="M30 13L40 23 40 13z"></path>
+      <path
+        fill="#e3f2fd"
+        d="M15 23H33V25H15zM15 27H33V29H15zM15 31H33V33H15zM15 35H25V37H15z"
+      ></path>
+    </svg>
+  )
+
+  return isFilterIcon ? filterIcon : defaultIcon
 }
--- src/components/icons/ImageIcon.tsx
+++ src/components/icons/ImageIcon.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
 
-export type ImageIconProps = React.SVGAttributes<SVGElement>
+export type ImageIconProps = React.SVGAttributes<SVGElement> & {
+  isFilterIcon?: boolean
+}
 
-export const ImageIcon: React.FC<ImageIconProps> = ({ ...props }) => {
-  return (
+export const ImageIcon: React.FC<ImageIconProps> = ({
+  isFilterIcon,
+  ...props
+}) => {
+  const defaultIcon = (
     <svg
       xmlns="http://www.w3.org/2000/svg"
       height="24px"
@@ -14,4 +19,19 @@ export const ImageIcon: React.FC<ImageIconProps> = ({ ...props }) => {
       <path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h560q33 0 56.5 23.5T840-760v560q0 33-23.5 56.5T760-120H200Zm0-80h560v-560H200v560Zm40-80h480L570-480 450-320l-90-120-120 160Zm-40 80v-560 560Z" />
     </svg>
   )
+
+  const filterIcon = (
+    <svg
+      width="100"
+      viewBox="0 0 1024 1024"
+      version="1.1"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M553.642667 387.072h229.034666l-229.034666-228.992v228.992M262.144 95.616h333.141333l249.856 249.813333v499.669334a83.285333 83.285333 0 0 1-83.285333 83.285333H262.186667a83.242667 83.242667 0 0 1-83.285334-83.285333V178.858667c0-46.208 37.034667-83.242667 83.285334-83.242667m0 749.525333h499.626666V512l-166.528 166.570667L512 595.285333l-249.856 249.856M345.429333 387.114667a83.285333 83.285333 0 0 0-83.285333 83.285333 83.285333 83.285333 0 0 0 83.285333 83.242667 83.285333 83.285333 0 0 0 83.285334-83.242667 83.285333 83.285333 0 0 0-83.285334-83.285333z"
+        fill="#26A69A"
+      />
+    </svg>
+  )
+  return isFilterIcon ? filterIcon : defaultIcon
 }
